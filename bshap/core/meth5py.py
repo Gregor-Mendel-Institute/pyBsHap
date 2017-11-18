fixed weighted average function

bshap/core/meth5py.py
@@ -233,7 +233,8 @@ def MethylationSummaryStats(meths, filter_pos_ix, category):
     if np.sum(mc_total) == 0:
         return(None)
     if category == 1:   # weighted mean
-        return np.divide(float(np.sum(mc_count)), np.sum(mc_total))
+        methylated_cs = np.where(meths.methylated[filter_pos_ix] == 1)[0]
+        return np.divide(float(np.sum(mc_count[methylated_cs])), np.sum(mc_total))
     elif category == 2: # fraction of methylated positions
         methylated = meths.methylated[filter_pos_ix]
         meths_len = np.sum(methylated)
@@ -264,15 +265,13 @@ def get_Methlation_required_bed(meths, required_bed, binLen, outmeths_avg, categ
 
 def get_Methlation_GenomicRegion(args):
     # bin_bed = Chr1,1,100
-    if args['required_region'] == '0,0,0':
-        outFile =  args['outFile'] + '.bedGraph'
+    if args['required_region'] == '0,0,0' or os.path.isfile(args['required_region']):
+        args['outFile'] =  args['outFile'] + '.bedGraph'
         if args['window_size'] is None:
-            window_size = 200
-        else:
-            window_size = int(args['window_size'])
-        run_bedtools.get_genomewide_methylation_WeightedMean(args['bedtoolsPath'], args['inFile'], outFile, window_size=window_size, overlap=args['overlap'], category = args['category'])
+            args['window_size'] = 200
+        run_bedtools.get_genomewide_methylation_WeightedMean(args)
         log.info("finished!")
-        return 0
+        return(0)
     if args['allc_path'] is None:
         log.info("reading hdf5 file!")
         meths = load_hdf5_methylation_file(args['inFile'])

bshap/core/run_bedtools.py
@@ -6,6 +6,7 @@ import numpy as np
 from subprocess import Popen, PIPE
 import logging
 import itertools
+import pybedtools
 
 log = logging.getLogger(__name__)
 chrs = ['Chr1','Chr2','Chr3','Chr4','Chr5']
@@ -22,7 +23,7 @@ def windows(seqlength, window_size, overlap):
         for x in range(1, seqlength, window_size):
             yield([x, x + window_size - 1])
 
-def generate_window_file(window_size, out_windows, overlap):
+def generate_window_file(window_size, overlap, out_windows):
     if os.path.isfile(out_windows):
         log.info("utilizing window file: %s" % out_windows)
         return(0)
@@ -36,7 +37,17 @@ def generate_window_file(window_size, out_windows, overlap):
     log.info("done!")
     return(0)
 
-def MethylationSummaryStats(window_file, bedFile, bedtoolPath, category):
+def get_weighted_average(feature):
+    """
+    Parsing the output of bedtools to get weighted average
+    """
+    if feature[3] != '.':
+        if int(feature[3]) > 0:
+            num_c = np.sum(np.array(feature[4].split(","), dtype=int) * np.array(feature[5].split(","), dtype=float))
+            return(num_c / int(feature[3]))
+    return(None)
+
+def MethylationSummaryStats(window_file, bedFile, bedtoolsPath, category):
     ## In a bedfile you have these columns
     # Chr1	22	23	CCC	0	+	1	1
     # Chr1	23	24	CCT	0	+	1	1
@@ -46,33 +57,43 @@ def MethylationSummaryStats(window_file, bedFile, bedtoolPath, category):
     # Chr1	32	33	CTG	0	+	1	1
     # Chr1	34	35	CAG	1	-	9	18
     bedtools_command = 'bedtools map -a ' + window_file + ' -b ' + bedFile
-    if bedtoolPath is not None:
-        bedtools_command = bedtoolPath + '/' + bedtools_command
+    if bedtoolsPath is not None:
+        bedtools_command = bedtoolsPath + '/' + bedtools_command
     skip_na_lines = ' | awk \'$4 != "." {print $0}\''
     if category == 1:   # weighted mean
-        bedtools_command = bedtools_command + ' -o sum,sum -c 7,8'
-        awk_command = '| awk \'$5 > 0 {print $1 "\t" $2 "\t" $3 "\t" $4/$5}\''
-        return(bedtools_command + awk_command + skip_na_lines)
+        bedtools_command = bedtools_command + ' -o sum,collapse,collapse -c 8,5,7'
+        return(bedtools_command)
     elif category == 2:  # fraction of methylated positions
         bedtools_command = bedtools_command + ' -o mean -c 5'
         return(bedtools_command + skip_na_lines)
     elif category == 3:  # absolute means
         awk_command = 'awk \'$8 > 0 {print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $7/$8 "\t" $6}\' ' + bedFile
         bedtools_command =  'bedtools map -a ' + window_file + ' -b stdin -o mean -c 5'
-        if bedtoolPath is not None:
-            bedtools_command = bedtoolPath + '/' + bedtools_command
+        if bedtoolsPath is not None:
+            bedtools_command = bedtoolsPath + '/' + bedtools_command
         return(awk_command + ' | ' + bedtools_command + skip_na_lines)
     else:
         raise(NotImplementedError)
 
-def get_genomewide_methylation_WeightedMean(bedtoolPath, bedFile, outFile, window_size, overlap, category):
-    outBedGraph = open(outFile, "w")
-    window_file = "tair10." + str(window_size) + "bp_windowsize." + str(overlap) + "bp_overlap.windows.txt"
-    generate_window_file(window_size, window_file, overlap)
-    full_command = MethylationSummaryStats(window_file, bedFile, bedtoolPath, category)
+def get_genomewide_methylation_WeightedMean(args):
+    category = args['category']
+    outBedGraph = open(args['outFile'], "w")
+    window_file = "tair10." + str(args['window_size']) + "bp_windowsize." + str(args['overlap']) + "bp_overlap.windows.txt"
+    generate_window_file(args['window_size'], args['overlap'], window_file)
+    full_command = MethylationSummaryStats(window_file, args['inFile'], args['bedtoolsPath'], args['category'])
     log.info("make sure the bedtools version is > v2.26.0")
     log.info('running bedtools!')
-    convertcsv = Popen(full_command, shell=True, stdout = outBedGraph)
-    convertcsv.wait()
-    outBedGraph.close()
-    log.info('done!')
+    if category != 1:
+        convertcsv = Popen(full_command, shell=True, stdout = outBedGraph)
+        convertcsv.wait()
+        outBedGraph.close()
+        log.info("done!")
+    elif category == 1:
+        convertcsv = Popen(full_command, shell=True, stdout = PIPE)
+        for each_line in iter(convertcsv.stdout.readline, ''):
+            each_bed = each_line.rstrip().split("\t")
+            score = get_weighted_average(each_bed)
+            if score is not None:
+                outBedGraph.write("%s\t%s\t%s\t%s\n" % (each_bed[0], each_bed[1], each_bed[2], score))
+        outBedGraph.close()
+        log.info('done!')